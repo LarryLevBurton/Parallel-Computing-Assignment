77138ec6a2dd025dc835eab7950a187b8ea176fd
Laurence Lev Burton <larry13@live.co.uk>
2018-12-06 00:28:24 +0000

    Change key to 969696

--- encryption-mp.c
+++ encryption-mp.c
@@ -11,7 +11,7 @@ int main (void)
    * real application? :-)
    */
   /* A 128 bit key */
-  unsigned char *key = (unsigned char *)"#####999999#####";
+  unsigned char *key = (unsigned char *)"#####969696#####";
   // unsigned char *key = (unsigned char *)"#####969249#####";
   // unsigned char *key = (unsigned char *)"0123456789012345";
   unsigned char decKey[17] = "#####000000#####";
